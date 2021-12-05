39cda11f1448cd2688e022bd827ba89b03f9d70c
crustyBobe <crusty_bobe@protonmail.com>
2021-12-04 17:15:53 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQExample
+{
+    class Program
+    {
+        static string[] names = { "Chad", "Lexi", "Matthew", "Bubbles", "Frank", "Justin", "Reuben", "Mark"};
+
+        static void Main(string[] args)
+        {
+            DisplayNamesThatStartWithM();
+            OrderNames();
+        }
+        static void DisplayNamesThatStartWithM()
+        {
+            var selectedNames =
+                from name in names
+                where name[0] == 'M'
+                select name;
+
+            foreach(var name in selectedNames)
+            {
+                Console.WriteLine(name);
+            }
+        }//end DisplayNamesThatStartWithM.
+
+
+        static void OrderNames()
+        {
+            var orderedNames =
+                from name in names
+                where name.Length > 5
+                where name [4] == 'a'
+                orderby name ascending
+                select name;
+
+            foreach(var name in orderedNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
